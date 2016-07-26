98ceeec8f771d3b2703752047027fbc9facc397e
Shilpika S <sshilpika5@gmail.com>
2016-07-26 08:31:56 -0500

    gethostbyname

--- parameterwebsocketclient.py
+++ parameterwebsocketclient.py
@@ -50,8 +50,8 @@ class TensorSparkWorker(Borg):
       ip=socket.gethostbyname(socket.gethostname())
       print(str(ip)+" IP address")
       print("WEB SOCKET PORT "+str(self.websocket_port))
-      print("ws://%s:%d/" % (ip,self.websocket_port))
-      self.websock = yield tornado.websocket.websocket_connect("ws://%s:%d/" % (ip,self.websocket_port), connect_timeout=3600)
+      #print("ws://%s:%d/" % (ip,self.websocket_port))
+      self.websock = yield tornado.websocket.websocket_connect("ws://10.233.100.210:%d/" % (self.websocket_port), connect_timeout=3600)
 
    def train_partition(self, partition):
       while True:
